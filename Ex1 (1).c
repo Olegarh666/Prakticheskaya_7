a205ac8bf5f2042ff5616368f57596274cdcc7bb
Olegarh666 <91975189+Olegarh666@users.noreply.github.com>
2024-11-29 10:51:58 +0300

    Add files via upload

---
+++ Ex1 (1).c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int secret_number;
+pid_t child_pid;
+int attempts; // Переменная для отслеживания количества попыток
+
+void handler_result(int sig) {
+    if (sig == SIGUSR1) {
+        printf("Игрок 2 угадал число %d!\n", secret_number);
+        printf("Количество попыток: %d\n", attempts);
+        exit(0); // Завершить процесс после успешного угадывания
+    } else if (sig == SIGUSR2) {
+        printf("Игрок 2 не угадал. Попробуйте снова.\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Использование: %s <N>\n", argv[0]);
+        return 1;
+    }
+
+    int N = atoi(argv[1]);
+    
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+
+    for (int game = 0; game < 10; game++) { // 10 игр
+        secret_number = rand() % N + 1; // Генерация случайного числа от 1 до N
+        printf("Игрок 1 загадывает число: %d\n", secret_number);
+
+        child_pid = fork();
+        
+        if (child_pid < 0) {
+            perror("Ошибка fork");
+            exit(1);
+        } else if (child_pid == 0) { // Процесс игрока 2
+            signal(SIGUSR1, handler_result);
+            signal(SIGUSR2, handler_result);
+
+            attempts = 0; // Сброс количества попыток
+            int guess;
+            while (1) {
+                guess = rand() % N + 1; // Генерация случайного предположения от 1 до N
+                attempts++; // Увеличиваем счетчик попыток
+                printf("Игрок 2 пытается угадать: %d\n", guess);
+
+                // Отправляем сигнал о том, что игрок 2 сделал предположение
+                if (guess == secret_number) {
+                    kill(getppid(), SIGUSR1); // Угадал
+                } else {
+                    kill(getppid(), SIGUSR2); // Не угадал
+                }
+
+                pause(); // Ожидание сигнала
+            }
+            exit(0);
+        } else { // Процесс игрока 1
+            signal(SIGUSR1, handler_result);
+	    signal(SIGUSR2, handler_result);
+	    while(1) {
+		pause();
+		if (waitpid(child_pid, NULL, WNOHANG) > 0) {
+		    break;
+		}
+	    } 
+            printf("GAME END. \n"); 
+        }
+    }
+
+    return 0;
+}
+
+
---
+++ Ex2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+
+#define MAX_TRIES 100
+
+struct msg_buffer {
+    long msg_type;
+    int guess;
+};
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Использование: %s <N>\n", argv[0]);
+        return 1;
+    }
+
+    int N = atoi(argv[1]);
+    srand(time(NULL));
+
+    key_t key = ftok("msgfile", 65); // Создание уникального ключа
+    int msgid = msgget(key, 0666 | IPC_CREAT); // Создание очереди сообщений
+
+    for (int game = 0; game < 10; game++) { // 10 игр
+        int secret_number = rand() % N + 1; // Генерация случайного числа от 1 до N
+        printf("Игрок 1 загадывает число: %d\n", secret_number);
+
+        if (fork() == 0) { // Процесс игрока 2
+            struct msg_buffer msg;
+            int attempts = 0;
+
+            while (1) {
+                msgrcv(msgid, &msg, sizeof(msg.guess), 1, 0); // Получение предположения
+                attempts++;
+                printf("Игрок 2 пытается угадать: %d\n", msg.guess);
+
+                if (msg.guess == secret_number) {
+                    printf("Игрок 2 угадал число %d!\n", secret_number);
+                    printf("Количество попыток: %d\n", attempts);
+                    exit(0);
+                } else {
+                    printf("Игрок 2 не угадал. Попробуйте снова.\n");
+                }
+            }
+        } else { // Процесс игрока 1
+            struct msg_buffer msg;
+            for (int i = 0; i < MAX_TRIES; i++) {
+		msg.msg_type = 1;
+		msg.guess = rand() % N + 1;
+		msgsnd(msgid, &msg, sizeof(msg.guess), 0);
+		sleep(1);
+	    }
+	    wait(NULL);
+	    printf("GAME END!!! \n");
+	}
+   }
+   msgctl(msgid, IPC_RMID, NULL);
+   return 0;
+} 
